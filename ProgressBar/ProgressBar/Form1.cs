8c7ad7c9d49a5b75952ae7917c16d5462ea6e6f5
Vaishnavi-Aringale <vaishnaviaringale23@gmail.com>
2026-01-16 16:03:36 +0530

    Add files via upload

---
+++ ProgressBar/ProgressBar/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void setTimerStatus(bool status)
+        {
+            timer1.Enabled = status;
+        }
+
+        public void setProgressLabel(string labelText)
+        {
+            label1.Text = labelText;
+        }
+
+        public void setSetpSize(int stepSize)
+        {
+            progressBar1.Step = stepSize;
+        }
+        public void setInterval(double secs)
+        {
+            timer1.Interval = (int)(1000 * secs);
+        }
+
+        public void incrementProgressBar()
+        {
+            progressBar1.PerformStep();
+            label2.Text = progressBar1.Value.ToString() + "%";
+            if (progressBar1.Value >= 100)
+            {
+                label1.Text = "Features Extracted Successfully!";
+                this.Close();
+            }
+        }
+
+        private void progressTimer_Tick(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+            if (progressBar1.Value >= 100)
+            {
+                label2.Text = "100%";
+            }
+            else
+                label2.Text = string.Format("{0:0}%", progressBar1.Value);
+        }
+    }
+}
---
+++ ProgressBar/ProgressBar/ProgressController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public static class ProgressController
+    {
+        private static Thread progressThread;
+        private static ProgressForm progressWindow;
+
+        // =========================
+        // START PROGRESS BAR
+        // =========================
+        public static void StartProgressBar(
+            int stepSize = 0,
+            double interval = 0.0,
+            bool timerStatus = false,
+            string progressLabel = "")
+        {
+            progressThread = new Thread(() =>
+            {
+                progressWindow = new ProgressForm();
+
+                if (stepSize > 0)
+                    progressWindow.setSetpSize(stepSize);
+
+                if (interval > 0.0)
+                    progressWindow.setInterval(interval);
+
+                if (timerStatus)
+                    progressWindow.setTimerStatus(timerStatus);
+
+                if (!string.IsNullOrEmpty(progressLabel))
+                    progressWindow.setProgressLabel(progressLabel);
+
+                Application.Run(progressWindow); // message loop
+            });
+
+            progressThread.SetApartmentState(ApartmentState.STA);
+            progressThread.IsBackground = true;
+            progressThread.Start();
+        }
+
+        // =========================
+        // INCREMENT PROGRESS
+        // =========================
+        public static void IncrementProgressBar()
+        {
+            if (progressWindow != null && progressWindow.IsHandleCreated)
+            {
+                progressWindow.Invoke(new Action(() =>
+                {
+                    progressWindow.incrementProgressBar();
+                }));
+            }
+        }
+
+        // =========================
+        // STOP PROGRESS BAR
+        // =========================
+        public static void StopProgressBar()
+        {
+            if (progressWindow != null && progressWindow.IsHandleCreated)
+            {
+                progressWindow.Invoke(new Action(() =>
+                {
+                    progressWindow.setProgressLabel("Completed!");
+                    Thread.Sleep(2000);
+                    progressWindow.Close();
+                }));
+
+                progressThread.Join();
+                progressWindow.Dispose();
+
+                progressWindow = null;
+                progressThread = null;
+            }
+        }
+    }
+}
